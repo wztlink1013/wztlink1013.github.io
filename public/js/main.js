fed65c318fd2f78c428f419d016a796f560129ce
wztlink1013 <2550374815@qq.com>
2020-11-28 02:17:38 +0000

    delete sougou automatically push from Github Actions at 2020-11-28 02:16:09

--- public/js/main.js
+++ public/js/main.js
@@ -1035,10 +1035,10 @@ $(window).on("load", function () {
       s.parentNode.insertBefore(bp, s);
     })();
     // TODO: 搜狗收录，自动推送
-    (function(){
-      var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
-      document.write('<script src="' + src + '" id="sozz"><\/script>');
-      })();
+    // (function(){
+    //   var src = "https://jspassport.ssl.qhimg.com/11.0.1.js?d182b3f28525f2db83acfaaf6e696dba";
+    //   document.write('<script src="' + src + '" id="sozz"><\/script>');
+    //   })();
     // TODO: 谷歌分析
     (function (i, s, o, g, r, a, m) {
       i['GoogleAnalyticsObject'] = r;
