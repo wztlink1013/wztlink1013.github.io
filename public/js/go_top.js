1145338617373e67858b84a10c10ece27a17f599
wztlink1013 <2550374815@qq.com>
2021-04-09 01:53:32 +0000

    update scrool addition from Github Actions at 2021-04-09 01:51:08

--- public/js/go_top.js
+++ public/js/go_top.js
@@ -3,9 +3,20 @@ var go_top = document.querySelector('#go_top');
 // 滚动显示go_top按钮
 // TODO: 缓慢显示
 
+
+
+// 使用的时候  getScroll().left
+
 window.addEventListener('scroll', function(){
-    // 持续监听返回顶端事件
-    if (document.documentElement.scrollTop !== 0) {
+    // 给出scroolTop一种兼容性方案
+    function getScroll() {
+        return {
+          left: window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft||0,
+          top: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+        };
+     } 
+    // 持续监听滚动状态
+    if (getScroll().top !== 0) {
         go_top.style.display = 'block';
     } else {
         go_top.style.display = 'none';
